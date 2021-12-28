8b1095fcbd54dd0a03ae6f45eade65f34e432af4
Crowdbotics <engineering@crowdbotics.com>
2021-12-28 07:20:13 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -10,6 +10,7 @@ export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   },
+
   "@modules/login": {
     LOGO_URL:
       "https://crowdbotics-slack-dev.s3.amazonaws.com/media/project_component_resources/cb-icon.png",
